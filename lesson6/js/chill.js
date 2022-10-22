9e993d2978d4b1ebe9952e24111741ef84f8635e
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2022-10-22 21:36:24 +0000

    Update chill.js

--- lesson6/js/chill.js
+++ lesson6/js/chill.js
@@ -12,5 +12,23 @@ if (tempNumber<=50 && speedNumber>3){
     document.getElementById("chill").textContent = "wind chill is "+windchill+"\xB0F";
 
 } else{
-document.getElementById("chill").textContent = "No windchill today"
+document.getElementById("chill").textContent = "Windchill: N/A"
 }
+
+const apiURL = "//api.openweathermap.org/data/2.5/weather?q=accra&units=imperial&appid=44af6e0267e626fb09390194949d7a28" ;
+
+ fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#temp').textContent = jsObject.main.temp;
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+    const sped = jsObject.wind.speed;
+    document.querySelector('#speed').textContent = sped;
+  });
+
