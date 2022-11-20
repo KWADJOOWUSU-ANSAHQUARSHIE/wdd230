ba24e62bbf79379a2eca6aed29e2a178ed8eed9b
KWADJOOWUSU-ANSAHQUARSHIE <97170718+KWADJOOWUSU-ANSAHQUARSHIE@users.noreply.github.com>
2022-11-20 20:03:49 +0000

    Add files via upload

---
+++ lesson11/js/form-check.js
@@ -0,0 +1,7 @@
+// Form-Check
+
+const cD = new Date();
+const fDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(cD);
+
+const currentHr = cD.getHours() + ':' + cD.getMinutes();
+document.getElementById('t_date').value = `${fDate} ${currentHr}`
